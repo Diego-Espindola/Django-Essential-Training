5b03116fe5866e8beb7c270c25e4832492bf8405
Diego Espindola <diegoespindola9404@gmail.com>
2024-04-04 21:39:23 -0300

    Creating a dynamic template, handling the 'not found' error, displaying content of a single note

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.list_items)
+    path('notes', views.list_items),
+    path('notes/<int:pk>', views.detail),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 
 from .models import Notes
 
@@ -6,3 +7,13 @@ from .models import Notes
 def list_items(request):
     all_notes = Notes.objects.all()
     return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+
+def detail(request, pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        # raise Http404("Note doesn't exist")
+        url = request.build_absolute_uri()
+        return render(request, 'notes/404_error.html', {'pk': pk, 'url': url})
+    return render(request, 'notes/notes_detail.html', {'note': note})
